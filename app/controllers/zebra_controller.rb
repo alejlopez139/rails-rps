fbfffe5c2c6298c762f8005267753b43f01f3c01
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-26 15:39:57 +0000

    paper routes work

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,11 +1,14 @@
 class ZebraController < ApplicationController
-  render({:template => "game_templates/layout"})
   def giraffe
+    @opp_hand = rand(1..3)
     render({ :template => "game_templates/play_rock" })
   end
   def rules
-    
     render({:template => "game_templates/rules"})
+  end
+  def paper
+    @opp_hand = rand(1..3)
+    render({:template => "game_templates/play_paper"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get("/rock", {:controller => "zebra", :action => "giraffe"}) 
   get("/" , {:controller => "zebra", :action =>"rules"})
+  get("/paper", {:controller => "zebra", :action => "paper"})
 end
